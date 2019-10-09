7dfeda8cb094421dd2567232a89a839ddd101cb5
Jose Martinez <jalejandromg@live.com>
2019-10-09 01:07:13 -0600

    UI - JavaSwing
    Aqui esta la maqueta que vamos a usar :)

---
+++ UI/BD_PRO_1/src/bd_pro_1/main.java
@@ -0,0 +1,10 @@
+package bd_pro_1;
+public class main {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+    
+}
---
+++ UI/BD_PRO_1/src/connect/ConnectBD.java
@@ -0,0 +1,6 @@
+package connect;
+public class ConnectBD {
+    
+    
+    
+}
