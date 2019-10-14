40b53d70f1dd47c991017c20c8cce4582c9e0565
nick <nicolas01001110@gmail.com>
2019-10-14 08:19:53 -0600

    INGRESO DEL AVANCE DE LAS CONSULTAS DE LAS GRAFICAS

--- SQL/ConsultaReportes.sql
+++ SQL/ConsultaReportes.sql
@@ -5,12 +5,37 @@ CREATE OR REPLACE PROCEDURE REP_PassChange_PR
     PfirstLast in VARCHAR2 DEFAULT '',
     PsecondLast in VARCHAR2 DEFAULT '',
     Pid in NUMBER DEFAULT 0,
-    PassChange_CURSOR out sys_refcursor
+    PassChange_CURSOR out sys_refcursor,
+    total out NUMBER
 )
 AS
 BEGIN
     OPEN PassChange_CURSOR FOR
     SELECT A.ID_LOG_IN,p.name,p.first_lastname,p.second_lastname,p.id_person from
+    (select id_log_in from password_changes pl
+    where (sysdate-pl.change_date) > 10 ) a
+    join Log_In lg on a.id_log_in = LG.Username
+    join PERSON p on lg.Username = p.id_log_in
+    where (a.id_log_in = Pusername OR Pusername = '') AND
+    (p.first_lastname = PfirstLast OR PfirstLast = '') AND
+    (p.second_lastname = PsecondLast OR PsecondLast = '') AND
+    (p.id_person = Pid OR Pid = 0);
+
+
+END;
+/
+
+CREATE OR REPLACE PROCEDURE REP_TotPassChange_PR
+(
+    Pusername in VARCHAR2 DEFAULT '',
+    PfirstLast in VARCHAR2 DEFAULT '',
+    PsecondLast in VARCHAR2 DEFAULT '',
+    Pid in NUMBER DEFAULT 0,
+    total out NUMBER
+)
+AS
+BEGIN
+    SELECT COUNT(a.id_log_in) into total from
     (select id_log_in from password_changes pl
     where (sysdate-pl.change_date) > 10 ) a
     join Log_In lg on a.id_log_in = LG.Username
@@ -69,6 +94,31 @@ BEGIN
  END;
  /
 
+ CREATE OR REPLACE PROCEDURE REP_PROPXCATEGTOTAL_PR
+(
+    PCOM in NUMBER DEFAULT 0,
+    PVOTES in NUMBER DEFAULT 0,
+    PCATEGORY in NUMBER DEFAULT 0,
+    PINICIALDATE in DATE DEFAULT TO_DATE('1999/00/00','YYYY/MM/DD'),
+    PDAYSBETWEEN in number DEFAULT 99999,
+    total out NUMBER
+)
+AS
+BEGIN
+    SELECT COUNT(PROP.A) INTO total FROM (SELECT P.ID_PROPOSAL A, COUNT(V.ID_PROPOSAL) B FROM PROPOSAL P
+        LEFT JOIN VOTE V ON P.ID_PROPOSAL = V.ID_PROPOSAL
+        JOIN PERSON PER ON P.ID_PERSON = PER.ID_PERSON 
+        WHERE PER.ID_COMMUNITY = PCOM OR PCOM = 0
+        GROUP BY P.ID_PROPOSAL
+        HAVING COUNT(V.ID_PROPOSAL) >= PVOTES
+        ORDER BY P.ID_PROPOSAL) PROP
+    JOIN PROPOSAL PROPO ON A=PROPO.ID_PROPOSAL
+    LEFT JOIN PROPOSALXCATEGORY PXC ON A = PXC.ID_PROPOSAL
+    JOIN CATEGORY C ON PXC.ID_CATEGORY=C.ID_CATEGORY
+    WHERE (C.ID_CATEGORY=PCATEGORY OR PCATEGORY = 0) AND (PROPO.POST_TIME>=PINICIALDATE AND PROPO.POST_TIME<=PINICIALDATE+PDAYSBETWEEN);  
+ END;
+ /
+
  /*Reporte de usuarios dos*/
 CREATE OR REPLACE PROCEDURE REP_TOPNPROP_PR
 (
@@ -84,7 +134,7 @@ BEGIN
     GROUP BY P.ID_PROPOSAL
     ORDER BY B DESC;
 END;
- /*METER EN PROCEDURE*/
+ /*REPORTE USUARIO TRES*/
 CREATE OR REPLACE PROCEDURE REP_OwnProp_PR
 (
     NPer in Number default 1,
