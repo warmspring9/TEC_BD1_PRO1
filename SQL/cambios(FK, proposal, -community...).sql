fdb3957891794fe9e69141649b270babff9cf7da
nick <nicolas01001110@gmail.com>
2019-10-14 20:23:09 -0600

    cambios a community y a district X
    se cambio community y se borro district

--- SQL/cambios(FK, proposal, -community...).sql
+++ SQL/cambios(FK, proposal, -community...).sql
@@ -1,10 +1,10 @@
 alter table person modify id_person number(9,0);
 alter table proposal modify id_person number(9,0);
-alter table log_in modify id_log_in number(9,0);
-alter table person modify id_log_in number(9,0);
-ALTER TABLE PERSON ADD CONSTRAINT PERSON_DISTRICT_FK FOREIGN KEY (ID_DISTRICT) REFERENCES DISTRICT(ID_DISTRICT);
-alter table proposal add id_district number(6);
-alter table proposal add constraint district_proposal_fk foreign key (id_district) references district(id_district);
+alter table log_in modify USERNAME number(9,0);
+alter table person modify ID_LOG_IN number(9,0);
+ALTER TABLE COMMUNITY
+	ADD CONSTRAINT COMMUNITY_CANTON_FK FOREIGN KEY
+	(ID_CANTON) REFERENCES CANTON(ID_CANTON);
 
 
 
